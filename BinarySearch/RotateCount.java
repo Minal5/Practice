e1eae72672e3252da32383f9617336f5385f700d
Minal Madhur <73118982+Minal5@users.noreply.github.com>
2022-01-09 23:02:33 +0530

    Number of times array is rotated
    In this question we have to find how many times the sorted array is rotated.

---
+++ BinarySearch/RotateCount.java
@@ -0,0 +1,102 @@
+package basics;
+
+public class RotateCount {
+
+	public static void main(String[] args) {
+		int[] arr= {4,5,6,7,0,1,2};
+		System.out.println(countRotations(arr));
+
+	}
+
+	private static int countRotations(int[] arr) {
+		int pivot=findPivot(arr);
+		return pivot+1;
+	}
+	
+	//Use this for non-duplicates
+	static int findPivot(int[] arr)
+	{
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end)
+		{
+			int mid=start +(end-start)/2;
+			//4 cases
+			//case 1:
+			if(mid<end && arr[mid]>arr[mid+1])
+			{
+				return mid;
+			}
+			
+			//case 2:
+			if(mid>start && arr[mid]<arr[mid-1])
+			{
+				return mid-1;
+			}
+			
+			//case 3:
+			else if(arr[mid]<=arr[start])
+			{
+				end=mid-1;
+			}
+			
+			//case 4:
+			else
+			{
+				start=mid+1;
+			}
+		}
+		return -1;
+	}
+	//Use this with duplicates
+	static int findPivotWithDuplicates(int[] arr)
+	{
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end)
+		{
+			int mid=start +(end-start)/2;
+			//4 cases
+			//case 1:
+			if(mid<end && arr[mid]>arr[mid+1])
+			{
+				return mid;
+			}
+			
+			//case 2:
+			if(mid>start && arr[mid]<arr[mid-1])
+			{
+				return mid-1;
+			}
+			
+			//if elements at middle,start,end are equal then just skip the duplicates
+			if(arr[mid]==arr[start] && arr[mid]==arr[end])
+			{
+				//skip the duplicates
+				//NOTE: what if these elements at start and end were the pivot??
+				//check if start is pivot
+				if(arr[start]>arr[start+1])
+					return start;
+				start++;
+				//check whether end is pivot
+				if(arr[end]<arr[end-1])
+					return end-1;
+				end--;
+			}
+			//left side is sorted, so pivot should be in right
+			else if(arr[start]<arr[mid]||(arr[start]==arr[mid] && arr[mid]>arr[end]))
+			{
+				start = mid+1;
+			}
+			else
+			{
+				end=mid-1;
+			}
+			
+			
+			
+		}
+		return -1;
+	}
+
+}
