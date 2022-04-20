b38c44169b973532264e239fb219e602a3d976b9
Minal Madhur <73118982+Minal5@users.noreply.github.com>
2022-04-21 00:30:16 +0530

    Cyclic Sort (for array of range [0,n] )
    Cyclic Sort is used only if the range of array is given between [0,n]. Time complexity in O(n)

---
+++ Searching & Sorting/CyclicSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+
+	public static void main(String[] args) {
+		int[] arr= {3,5,2,1,4};
+		cyclicSort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	static void cyclicSort(int[] nums)
+	{
+		int i=0;
+		while(i<nums.length)
+		{
+			int correct=nums[i]-1;
+			if(nums[i] != nums[correct])
+			{
+				swap(nums,i,correct);
+			}
+			else
+				i++;
+		}
+	}
+	static void swap(int[] arr,int first,int second)
+	{
+		int temp=arr[first];
+		arr[first]=arr[second];
+		arr[second]=temp;
+	}
+}
