89f6d1f9141e3882e67dade87f7b79d582dd405a
Minal Madhur <73118982+Minal5@users.noreply.github.com>
2022-11-08 16:35:28 +0530

    215. Kth Largest Element in an Array

---
+++ heap/KthLargestElement.java
@@ -0,0 +1,23 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+
+	public static void main(String[] args) {
+		int[] nums = {3,2,3,1,2,4,5,5,6};
+		int k = 4;
+		System.out.println(findKthLargest(nums,k));
+	}
+
+	public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++) {
+        	minHeap.add(nums[i]);
+        	if(minHeap.size() > k) {
+        		minHeap.poll();
+        	}
+        }
+        return minHeap.peek();
+    }
+}
