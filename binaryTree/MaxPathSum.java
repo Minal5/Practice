6240f65cd27a323f11a2befe913e01a15f2b020d
Minal Madhur <73118982+Minal5@users.noreply.github.com>
2022-12-10 18:26:56 +0530

    124. Binary Tree Maximum Path Sum

---
+++ binaryTree/MaxPathSum.java
@@ -0,0 +1,24 @@
+package binaryTree;
+
+public class MaxPathSum {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public int maxPathSum(TreeNode root) {
+		int[] maxval = new int[1];
+		maxval[0] = Integer.MIN_VALUE;
+		maxPathDown(root,maxval);
+		return maxval[0];
+	}
+	
+	private int maxPathDown(TreeNode node,int maxval[]) {
+		if(node == null) return 0;
+		int left = Math.max(0, maxPathDown(node.left,maxval));
+		int right = Math.max(0, maxPathDown(node.right,maxval));
+		maxval[0] = Math.max(maxval[0],left + right+node.val);
+		return Math.max(left, right)+node.val;
+	}
+
+}
